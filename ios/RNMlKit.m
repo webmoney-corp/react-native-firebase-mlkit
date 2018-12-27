94630dceb9c3987c7023e95b80b4b2e915c55a5a
sfjwr <s-fujiwara@esm.co.jp>
2018-12-27 15:57:10 +0900

    image-storeからの読み込みに対応

--- ios/RNMlKit.m
+++ ios/RNMlKit.m
@@ -5,8 +5,11 @@
 
 #import <FirebaseCore/FirebaseCore.h>
 #import <FirebaseMLVision/FirebaseMLVision.h>
+#import <React/RCTImageLoader.h>
 
 @implementation RNMlKit
+
+@synthesize bridge = _bridge;
 
 - (dispatch_queue_t)methodQueue
 {
@@ -23,69 +26,84 @@ RCT_REMAP_METHOD(deviceTextRecognition, deviceTextRecognition:(NSString *)imageP
     }
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        FIRVision *vision = [FIRVision vision];
-        FIRVisionTextRecognizer *textRecognizer = [vision onDeviceTextRecognizer];
-        NSDictionary *d = [[NSDictionary alloc] init];
-        NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:imagePath]];
-        UIImage *image = [UIImage imageWithData:imageData];
-        
-        if (!image) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                resolve(@NO);
-            });
-            return;
-        }
-        
-        FIRVisionImage *handler = [[FIRVisionImage alloc] initWithImage:image];
+            
+        [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:imagePath] callback:^(NSError *error, UIImage *image) {
 
-        [textRecognizer processImage:handler completion:^(FIRVisionText *_Nullable result, NSError *_Nullable error) {
-            if (error != nil || result == nil) {
-                NSString *errorString = error ? error.localizedDescription : detectionNoResultsMessage;
-                NSDictionary *pData = @{
-                                        @"error": [NSMutableString stringWithFormat:@"On-Device text detection failed with error: %@", errorString],
-                                        };
-                // Running on background thread, don't call UIKit
+            if (error || image == nil) {
+                if ([imagePath hasPrefix:@"data:"] || [imagePath hasPrefix:@"file:"]) {
+                    NSURL *imageUrl = [[NSURL alloc] initWithString:imagePath];
+                    image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
+                } else {
+                    image = [[UIImage alloc] initWithContentsOfFile:imagePath];
+                }
+                if (image == nil) {
+                    // callback(@[@"Can't retrieve the file from the path.", @""]);
+                    return;
+                }
+            }
+            
+            FIRVision *vision = [FIRVision vision];
+            FIRVisionTextRecognizer *textRecognizer = [vision onDeviceTextRecognizer];
+            NSDictionary *d = [[NSDictionary alloc] init];
+            
+            if (!image) {
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    resolve(pData);
+                    resolve(@NO);
                 });
                 return;
             }
-
-            CGRect boundingBox;
-            CGSize size;
-            CGPoint origin;
-            NSMutableArray *output = [NSMutableArray array];
-
-            for (FIRVisionTextBlock *block in result.blocks) {
-                NSMutableDictionary *blocks = [NSMutableDictionary dictionary];
-                NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
-                NSString *blockText = block.text;
-
-                blocks[@"resultText"] = result.text;
-                blocks[@"blockText"] = block.text;
-                blocks[@"bounding"] = bounding;
-                [output addObject:blocks];
-
-                for (FIRVisionTextLine *line in block.lines) {
-                    NSMutableDictionary *lines = [NSMutableDictionary dictionary];
-                    lines[@"lineText"] = line.text;
-                    [output addObject:lines];
-
-                    for (FIRVisionTextElement *element in line.elements) {
-                        NSMutableDictionary *elements = [NSMutableDictionary dictionary];
-                        elements[@"elementText"] = element.text;
-                        [output addObject:elements];
-
+            
+            FIRVisionImage *handler = [[FIRVisionImage alloc] initWithImage:image];
+            
+            [textRecognizer processImage:handler completion:^(FIRVisionText *_Nullable result, NSError *_Nullable error) {
+                if (error != nil || result == nil) {
+                    NSString *errorString = error ? error.localizedDescription : detectionNoResultsMessage;
+                    NSDictionary *pData = @{
+                                            @"error": [NSMutableString stringWithFormat:@"On-Device text detection failed with error: %@", errorString],
+                                            };
+                    // Running on background thread, don't call UIKit
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        resolve(pData);
+                    });
+                    return;
+                }
+                
+                CGRect boundingBox;
+                CGSize size;
+                CGPoint origin;
+                NSMutableArray *output = [NSMutableArray array];
+                
+                for (FIRVisionTextBlock *block in result.blocks) {
+                    NSMutableDictionary *blocks = [NSMutableDictionary dictionary];
+                    NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
+                    NSString *blockText = block.text;
+                    
+                    blocks[@"resultText"] = result.text;
+                    blocks[@"blockText"] = block.text;
+                    blocks[@"bounding"] = bounding;
+                    [output addObject:blocks];
+                    
+                    for (FIRVisionTextLine *line in block.lines) {
+                        NSMutableDictionary *lines = [NSMutableDictionary dictionary];
+                        lines[@"lineText"] = line.text;
+                        [output addObject:lines];
+                        
+                        for (FIRVisionTextElement *element in line.elements) {
+                            NSMutableDictionary *elements = [NSMutableDictionary dictionary];
+                            elements[@"elementText"] = element.text;
+                            [output addObject:elements];
+                            
+                        }
                     }
                 }
-            }
-
-            dispatch_async(dispatch_get_main_queue(), ^{
-                resolve(output);
-            });
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    resolve(output);
+                });
+            }];
         }];
     });
-    
+        
 }
 
 @end
